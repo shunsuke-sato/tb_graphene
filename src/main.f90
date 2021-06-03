cb286b5ad9d81f288208248bc2f906fc29bb9b7d
Shunsuke Sato <shunsuke.sato26@gmail.com>
2021-06-03 12:15:55 +0200

    Added inputoutput.f90

--- src/main.f90
+++ src/main.f90
@@ -3,10 +3,10 @@ program main
   implicit none
 
   call init_parallel
-
+  call init_inputoutput
   write(*,*)'myrank=',comm_id_global
 
-
+  call fin_inputoutput
   call fin_parallel
 
 end program main
